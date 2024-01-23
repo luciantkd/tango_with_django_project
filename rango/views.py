1a4b081d6976cdbea46405886523d4560fcd8dd4
luciantkd <lucianprocopciuc1@hotmail.com>
2024-01-23 17:13:57 +0000

    Updated urls.py and views.py with new view and URL mapping.

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,8 @@ def index(request):
     #create a string containing the hyperlink to the about page
     link_to_about = '<a href="/rango/about/">About</a>'
     #add the hyperlink to the HttpResponse
-    return HttpResponse(f"Rango says hey there partner! {link_to_about}")
+    return HttpResponse("Rango says hey there partner! "+ link_to_about)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    link_to_index = '<a href="/rango/">Index</a>'
+    return HttpResponse("Rango says here is the about page. " + link_to_index)
