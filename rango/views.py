3c6ac04a0ddc217f39c94d4e54f9b9fdf73aa9ab
luciantkd <lucianprocopciuc1@hotmail.com>
2024-01-23 13:45:57 +0000

    about link

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    #create a string containing the hyperlink to the about page
+    link_to_about = '<a href="/rango/about/">About</a>'
+    #add the hyperlink to the HttpResponse
+    return HttpResponse(f"Rango says hey there partner! {link_to_about}")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
