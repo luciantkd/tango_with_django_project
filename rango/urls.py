c48cd3b987176af2342f02c59b6a229561cefe05
luciantkd <lucianprocopciuc1@hotmail.com>
2024-01-30 17:18:27 +0000

    added about html page and cat image

--- rango/urls.py
+++ rango/urls.py
@@ -5,7 +5,7 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('', views.about, name='about'),
-    path('index/', views.index, name='index'),
 ]
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.about, name='about'),
     path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango
     path('admin/', admin.site.urls),
